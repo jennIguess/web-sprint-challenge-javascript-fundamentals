0e5947161c7004ace5f5f4122b14cc942a9e9174
Jennifer <jenniferpresleyvega@gmail.com>
2020-06-12 20:16:02 -0700

    completed closure.js, not confident on this one

--- challenges/closure.js
+++ challenges/closure.js
@@ -17,8 +17,17 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
-
+// nestedFunction console.logs internal within it's scope, so it's able to access the properties of internal. nestedFunction uses a closure to pull data from internal.
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+let num = [];
+const summation = function(x){
+  for (i = 0; i = x; i++){ 
+    num.push[i];
+
+  };
+  return num.reduce((a, b) => a + b, 0);
+};
+console.log(summation(4));
